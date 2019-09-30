5d1d10f774d2349e7f6b9e04d8cbdbc4c589d057
Robert Sohlberg <45578298+sohlbert@users.noreply.github.com>
2019-09-30 18:32:29 -0500

    Add files via upload

---
+++ First_Game.py
@@ -0,0 +1,24 @@
+#Making a game
+
+#Dictionary
+player = {'name': 'Robert', 'attack': 10, 'heal': 16, 'health': 100}
+monster = {'name': 'Chuthlu', 'attack': 12, 'health': 150}
+
+print("Please select action")
+print("1) Attack")
+print("2) Heal")
+
+player_choice = input()
+
+
+if player_choice == '1':
+    monster['health'] = monster['health'] - player['attack']
+    player['health'] = player['health'] - monster['attack']
+    print(monster['health'])
+    print(player['health'])
+
+
+elif player_choice == '2':
+    print("Healing")
+else:
+    print('Invalid Input')
