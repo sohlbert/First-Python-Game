fba853a8cc57949515aa39778a0421fee00aaa86
Robert Sohlberg <45578298+sohlbert@users.noreply.github.com>
2019-09-30 18:03:55 -0500

    Add files via upload
    making folder for files

---
+++ First-Python-Game/First_Game.py
@@ -0,0 +1,13 @@
+#Making a game
+#What do I want
+#Player Name, Attack, Monster, Monster name, if alive, attack, etc etc.
+
+#define the player
+# player_name = "Robert"
+# player_attack = 10
+# player_heal = 16
+# health = 100
+
+player = ["Robert", 10, 16, 100]
+
+print(player[2])
