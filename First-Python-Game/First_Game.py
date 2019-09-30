8d2b53c39f7b0eb6b67419d24d34c1cf8356bc62
Robert Sohlberg <45578298+sohlbert@users.noreply.github.com>
2019-09-30 18:39:13 -0500

    Add files via upload
    adding a while loop

--- First-Python-Game/First_Game.py
+++ First-Python-Game/First_Game.py
@@ -3,13 +3,15 @@
 #Dictionary
 player = {'name': 'Robert', 'attack': 10, 'heal': 16, 'health': 100}
 monster = {'name': 'Chuthlu', 'attack': 12, 'health': 150}
+game_running = True
+
+while game_running = True:
 
 print("Please select action")
 print("1) Attack")
 print("2) Heal")
 
 player_choice = input()
-
 
 if player_choice == '1':
     monster['health'] = monster['health'] - player['attack']
