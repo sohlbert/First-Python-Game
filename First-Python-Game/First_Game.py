c90abf5d451b635c4275e461e2d2df47dee3f93c
Robert Sohlberg <45578298+sohlbert@users.noreply.github.com>
2019-09-30 19:36:34 -0500

    Add files via upload

--- First-Python-Game/First_Game.py
+++ First-Python-Game/First_Game.py
@@ -5,22 +5,24 @@ player = {'name': 'Robert', 'attack': 10, 'heal': 16, 'health': 100}
 monster = {'name': 'Chuthlu', 'attack': 12, 'health': 150}
 game_running = True
 
-while game_running = True:
+while game_running == True:
 
-print("Please select action")
-print("1) Attack")
-print("2) Heal")
+    print("Please select action")
+    print("1) Attack")
+    print("2) Heal")
 
-player_choice = input()
+    player_choice = input()
 
-if player_choice == '1':
-    monster['health'] = monster['health'] - player['attack']
-    player['health'] = player['health'] - monster['attack']
-    print(monster['health'])
-    print(player['health'])
+    if player_choice == '1':
+        monster['health'] = monster['health'] - player['attack']
+        player['health'] = player['health'] - monster['attack']
+        print(monster['health'])
+        print(player['health'])
 
 
-elif player_choice == '2':
-    print("Healing")
-else:
-    print('Invalid Input')
+    elif player_choice == '2':
+        print("Healing")
+    else:
+        print('Invalid Input')
+    if player['health'] <= 0:
+            game_running = False
