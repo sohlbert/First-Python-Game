f598141c3b4fda410ac11b91bc12453975b789d1
Robert Sohlberg <45578298+sohlbert@users.noreply.github.com>
2019-09-30 20:06:22 -0500

    Add files via upload
    added rounds.  game no longer ends upon HP reaching 0 for either player or monster. next finish working on UI

--- First-Python-Game/First_Game.py
+++ First-Python-Game/First_Game.py
@@ -1,38 +1,54 @@
 #Making a game
-
-#Dictionary
-player = {'name': 'Robert', 'attack': 10, 'heal': 16, 'health': 100}
-monster = {'name': 'Chuthlu', 'attack': 12, 'health': 150}
 game_running = True
 
 while game_running == True:
+    new_round = True
+    player = {'name': 'Robert', 'attack': 10, 'heal': 16, 'health': 100}
+    monster = {'name': 'Chuthlu', 'attack': 12, 'health': 120}
+    
+    print('***' *7)
+    print('Enter Player Name')
+    player['name'] = input()
 
-    player_won = False
-    monster_won = False
+    while new_round == True:
 
-    print("Please select action")
-    print("1) Attack")
-    print("2) Heal")
+        player_won = False
+        monster_won = False
 
-    player_choice = input()
+        print('***' *7)
+        print('***' *7)
+        print('***' *7)
+        print("1) Attack")
+        print("2) Heal")
+        print('3) Exit Game')
+        print('***' *7)
+        print('***' *7)
+        print('***' *7)
 
-    if player_choice == '1':
-        monster['health'] = monster['health'] - player['attack']
-        if monster['health'] <=0:
-            player_won = True
+        player_choice = input()
+
+        if player_choice == '1':
+            monster['health'] = monster['health'] - player['attack']
+            if monster['health'] <=0:
+                player_won = True
+
+            else:
+                player['health'] = player['health'] - monster['attack']
+                if player['health'] <= 0:
+                    monster_won = True
+
+            print(monster['health'])
+            print(player['health'])
+
+        elif player_choice == '2':
+            print("Healing")
+
+        elif player_choice == '3':
+            new_round = False
+            game_running = False
 
         else:
-            player['health'] = player['health'] - monster['attack']
-            if player['health'] <= 0:
-                monster_won = True
+            print('Invalid Input')
 
-        print(monster['health'])
-        print(player['health'])
-
-    elif player_choice == '2':
-        print("Healing")
-    else:
-        print('Invalid Input')
-
-    if player_won == True or monster_won == True:
-            game_running = False
+        if player_won == True or monster_won == True:
+            new_round = False
